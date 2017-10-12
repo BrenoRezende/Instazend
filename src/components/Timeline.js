b7ff721d2e3c4a8ef6eb82aee33225fb7955771d
Breno Rezende <brenorrezende@gmail.com>
2017-10-12 15:07:42 -0300

    Splitting components

---
+++ src/components/Timeline.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Photo from './photo/Photo';
+
+export default class Timeline extends Component {
+   render() {
+      return (
+         <div className="fotos container">
+            <Photo />
+            <Photo />
+            <Photo />
+         </div>
+      );
+   }
+}
